a926df6eaf0a53e818f06c3a36a895b50d0e5126
Diego F. Gómez <gomezdiego1998@gmail.com>
2020-06-09 18:24:38 -0500

    add env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  keyGoogle: 'AIzaSyCxbj2nM1XFoxLSp6lIEMkI6CUG7ifddjg',
+  KeyMapboxComponent: 'pk.eyJ1IjoiZGllZ29nb21leiIsImEiOiJjam5qaW1xeXMwenNsM2tueXFiZWZ1andiIn0.suJs58a1pOpL4c-rQzU2VA',
+  KeyMapboxService: 'pk.eyJ1IjoianVhbm1vbnRlcyIsImEiOiJjajAycnNsMjcwN3J5MnBxcWZ0YXQ1MHczIn0.Nt6C9JdeIv44PUSAkuPiGg',
 };
