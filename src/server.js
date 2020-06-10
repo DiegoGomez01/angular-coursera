acb19ce3d66b2f288204e18b90e269a8a7c489fa
Diego F. Gómez <gomezdiego1998@gmail.com>
2020-06-09 19:03:02 -0500

    changes package

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(__dirname + '/dist/finalProject'));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/finalProject/index.html'));
-});
